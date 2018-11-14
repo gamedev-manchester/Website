f058dcef213f976ec6e5620ca79aca3e8b1b497f
Mohammad Patel <mohammad.patel@kyutel.com>
2018-11-14 20:34:37 +0000

    added instaram gallerey

--- helpers/GalleryHelpers.js
+++ helpers/GalleryHelpers.js
@@ -1,20 +1,17 @@
 // Helper functions for the Gallery page
 "use strict";
 
-// Maps Flickr photosets to gallery objects that can be displayed on the page
-exports.mapPhotosetsToGalleries = (photosets) => {
-  try {
-    const galleries = photosets.photoset.map(g => { // Map all sets
-      return { // Include only the required fields
-        title: g.title._content,
-        description: g.description._content,
-        link: `https://www.flickr.com/photos/${process.env.FLICKR_USER_ID}/sets/${g.id}`,
-        thumbUrl: g.primary_photo_extras.url_m
-      };
-    });
-
-    return galleries;
+exports.parseInstagramResponse = (data) => {
+  try{
+    const images = data.data.map(g => {
+      return {
+        description: g.caption.text,
+        link: g.images.standard_resolution.url,
+        thumbUrl: g.images.thumbnail.url
+      }
+    })
+    return images;
   } catch (err) {
-    throw new Error(`Could not convert photosets to galleries: ${err}`);
+    throw new Error(`Could not parse instagram response: ${err}`);
   }
-};
+}
--- services/GalleryService.js
+++ services/GalleryService.js
@@ -1,30 +1,33 @@
 "use strict";
 
 const Flickr = require("flickr-sdk");
+const Instagram = require('node-instagram').default;
+// var Instafeed = require("instafeed");
 
 const galleryHelpers = require('../helpers/GalleryHelpers');
 
 // Fetches the galleries from the Flickr API
 exports.getGalleries = (callback) => {
-  // Connecting to Flickr
-  const flickr = new Flickr(process.env.FLICKR_API_KEY);
 
-  // Fetchinfg the data from Flickr
-  flickr.photosets.getList({
-    api_key: process.env.FLICKR_API_KEY,
-    user_id: process.env.FLICKR_USER_ID,
-    primary_photo_extras: ["url_m"]
-  }).then(data => {
-    try {
-      // Processing the received data
-      const parsedData = JSON.parse(data.text);
-      const galleries = galleryHelpers.mapPhotosetsToGalleries(parsedData.photosets);
-
-      return callback(null, galleries);
-    } catch (err) {
-      return callback(`Could not parse Flickr API response: ${err}`, null);
-    }
-  }).catch(err => {
-    return callback(`Could download gallery from Flickr API: ${err}`, null);
+  const instagram = new Instagram({
+    clientId: process.env.INSTAGRAM_CLIENT_ID,
+    clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+    accessToken: process.env.INSTAGRAM_ACCESS_TOKEN,
   });
+
+  // Get the most recent media published by the owner of the access_token.
+  instagram.get('users/self/media/recent').then(data => {
+      try {
+          const galleries = galleryHelpers.parseInstagramResponse(data);
+          console.log(galleries,'oi')
+          return callback(null, galleries);
+      } catch (err) {
+          return callback(`Could not parse Instagram API response: ${err}`, null);
+      }
+  }).catch(err => {
+      return callback(`Could download gallery Instagram Flickr API: ${err}`, null);
+  });
+
 };
+
+
